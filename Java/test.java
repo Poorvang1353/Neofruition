ce98c2dfc79a62f8b2352614c662d7c054a3daa8
Poorvang1353 <poorvangpatel13@gmail.com>
2022-10-31 10:10:54 +0530

    commit

--- Java/test.java
+++ Java/test.java
@@ -1,48 +1,47 @@
 import java.util.Scanner;
+
 public class test {
-    static int findIndex(int n) 
-    { 
-        if (n < 1) { 
+    static int findIndex(int n) {
+        if (n < 1) {
             return n;
-        } 
-        if (n==1 || n==5) {
+        }
+        if (n == 1 || n == 5) {
             System.out.println(n);
             return n;
         }
-        int a = 0, b = 1, c = 1; 
-        int res = 1; 
-        while (c < n) 
-        { 
-            c = a + b; 
-            res++; 
-            a = b; 
-            b = c; 
+        int a = 0, b = 1, c = 1;
+        int res = 1;
+        while (c < n) {
+            c = a + b;
+            res++;
+            a = b;
+            b = c;
             // System.out.println(c);
-        } 
-        findSum(res); 
-        return res; 
-    } 
-    static int findSum(int n){
-        int m=n-1;
-        int a=0,b=1,c;
+        }
+        findSum(res);
+        return res;
+    }
+
+    static int findSum(int n) {
+        int m = n - 1;
+        int a = 0, b = 1, c;
         for (int j = 1; j <= m; j++) {
-            c=a+b;
-            a=b;
-            b=c;
+            c = a + b;
+            a = b;
+            b = c;
 
         }
-        
+
         System.out.print(a);
         return a;
     }
-    public static void main (String[] args)  
-    { 
+
+    public static void main(String[] args) {
         int result;
         Scanner foo = new Scanner(System.in);
         System.out.println("enter the number : ");
-        result=foo.nextInt();
-        findIndex(result); 
+        result = foo.nextInt();
+        findIndex(result);
         foo.close();
-    } 
-} 
-
+    }
+}
