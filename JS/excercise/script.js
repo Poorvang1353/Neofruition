09bbc63e02c822df3c73a52adf0e25762be1b1f0
Poorvang1353 <poorvangpatel13@gmail.com>
2022-12-22 17:00:01 +0530

    commit

--- JS/excercise/script.js
+++ JS/excercise/script.js
@@ -26,8 +26,8 @@ function insertNewRecord(data) {
     cell1.innerHTML = data.Name;
     cell2 = newRow.insertCell(1);
     cell2.innerHTML = data.mobileNo;
-    cell4 = newRow.insertCell(2);
-    cell4.innerHTML = data.emailId;
+    cell3 = newRow.insertCell(2);
+    cell3.innerHTML = data.emailId;
     cell4 = newRow.insertCell(3);
     cell4.innerHTML = `<a onClick="onEdit(this)">Edit</a>
                        <a onClick="onDelete(this)">Delete</a>`;
