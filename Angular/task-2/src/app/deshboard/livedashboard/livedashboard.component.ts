99da9f7568a49c0ee12007c2b8a059c3a91069fa
Poorvang1353 <poorvangpatel13@gmail.com>
2023-01-23 15:54:36 +0530

    commit

--- Angular/task-2/src/app/deshboard/livedashboard/livedashboard.component.ts
+++ Angular/task-2/src/app/deshboard/livedashboard/livedashboard.component.ts
@@ -14,6 +14,10 @@ export class livedashboardComponent implements OnInit {
   ngOnInit(): void {
     google.charts.load('current', { packages: ['corechart'] });
     google.charts.setOnLoadCallback(this.drawChart);
+
+    google.charts.load('current', {'packages':['corechart']});
+    google.charts.setOnLoadCallback(this.drawVisualization);
+
     this.bnIdle.startWatching(50).subscribe((isTimeOut: boolean) => {
       if (isTimeOut) {
         console.log('session expired');
@@ -47,7 +51,33 @@ export class livedashboardComponent implements OnInit {
     };
     var chart = new google.visualization.ColumnChart(document.getElementById('divPiechart'));
     chart.draw(data, options);
+    
   }
+
+  drawVisualization() {
+    // Some raw data (not necessarily accurate)
+    var data = google.visualization.arrayToDataTable([
+      ['Month', 'Bolivia', 'Ecuador', 'Madagascar', 'Papua New Guinea', 'Rwanda', 'Average'],
+      ['2004/05',  165,      938,         522,             998,           450,      614.6],
+      ['2005/06',  135,      1120,        599,             1268,          288,      682],
+      ['2006/07',  157,      1167,        587,             807,           397,      623],
+      ['2007/08',  139,      1110,        615,             968,           215,      609.4],
+      ['2008/09',  136,      691,         629,             1026,          366,      569.6]
+    ]);
+
+    var options = {
+      title : 'Monthly Production by Country',
+      hAxis: {title: 'Month'},
+      seriesType: 'bars',
+      series: {5: {type: 'line'}},
+      width: 500,
+      height: 200,
+    };
+
+    var chart = new google.visualization.ComboChart(document.getElementById('chart_div'));
+    chart.draw(data, options);
+  }
+
   chart: any;
   logout() {
     localStorage.removeItem('token');
--- Angular/task-3/src/app/login/login.component.ts
+++ Angular/task-3/src/app/login/login.component.ts
@@ -8,34 +8,13 @@ import { AuthServiceService } from '../auth-service.service';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  formGroup!: FormGroup;
-  users:any;
-  constructor(private authService:AuthServiceService){
-    this.authService.GetLoginDetails().subscribe((data)=>{
-      console.warn("data",data)
-      this.users=data;
-    })
+
+
+  constructor(){
+
   }
   ngOnInit() {
-      this.initForm
+
   }
-  initForm(){
-    this.formGroup = new FormGroup({
-      username: new FormControl('',[Validators.required]),
-      password: new FormControl('',[Validators.required])
-    })
-  }
-  loginProcess(){
-    if(this.formGroup.valid){
-      this.authService.login(this.formGroup.value).subscribe(result =>{
-        if(result.success){
-          console.log(result);
-          alert(result.message);
-        }
-        else{
-          alert(result.message);
-        }
-      })
-    }
-  }
+  
 }
